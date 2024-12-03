055134874207fd7d219f5d54a21d2d17de876266
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2024-12-03 22:35:38 +1100

    before swapping DbContext for IdentityContext

--- db-init.sh
+++ db-init.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 echo "Waiting for the database to be ready..."
-until pg_isready -h db -p 5432 -U admin; do
+until pg_isready -h arcadia_postgresql -p 5432 -U arcadia; do
     sleep 2
 done
 
 if [ "$RESET_DB" = "true" ]; then
     echo "Resetting the database..."
-    dotnet ef database drop --force --context Arcadia.API.Data.ApplicationDbContext &&
-    dotnet ef migrations remove --force --context Arcadia.API.Data.ApplicationDbContext &&
-    dotnet ef migrations add InitDb --context Arcadia.API.Data.ApplicationDbContext --output-dir Migrations &&
-    dotnet ef database update --context Arcadia.API.Data.ApplicationDbContext
+    dotnet ef database drop --force --context Arcadia.API.Data.ApplicationDbContext --project /source/Arcadia.API &&
+    dotnet ef migrations remove --force --context Arcadia.API.Data.ApplicationDbContext --project /source/Arcadia.API &&
+    rm -rf /source/Arcadia.API/Migrations &&
+    dotnet ef migrations add InitDb --context Arcadia.API.Data.ApplicationDbContext --output-dir Migrations --project /source/Arcadia.API &&
+    dotnet ef database update --context Arcadia.API.Data.ApplicationDbContext --project /source/Arcadia.API
 else
     echo "Running migrations..."
-    dotnet ef migrations add InitDb --context Arcadia.API.Data.ApplicationDbContext --output-dir Migrations &&
-    dotnet ef database update --context Arcadia.API.Data.ApplicationDbContext
+    dotnet ef migrations add InitDb --context Arcadia.API.Data.ApplicationDbContext --output-dir Migrations --project /source/Arcadia.API &&
+    dotnet ef database update --context Arcadia.API.Data.ApplicationDbContext --project /source/Arcadia.API
 fi
 
 echo "Done."
